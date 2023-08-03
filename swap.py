67bfc0c0c9377fa881be138fa4eed9c962ef440f
37Yash <128834519+37Yash@users.noreply.github.com>
2023-08-03 11:50:09 +0530

    Add files via upload

---
+++ swap.py
@@ -0,0 +1,7 @@
+a=int(input("Enter the value of a : "))
+b=int(input("Enter the value of b : "))
+print("Value before swapping : ")
+print(" A= ",a, " and B = " ,b)
+b,a=a,b
+print ("Value after Swapping : ")
+print("A = ",a," and B = ",b)
---
+++ swappingdifferent.py
@@ -0,0 +1,6 @@
+a=int(input("Enter the a : "))
+b=int(input("Enter the b : "))
+a=a*b
+b=a//b
+a=a//b
+print("Values of a and b " ,a,"&" ,b)
